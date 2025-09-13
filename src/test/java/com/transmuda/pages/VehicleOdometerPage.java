198a009e7b018d2cd712725551be5dfb57e963e5
Shaistar88 <shaistar88@gmail.com>
2025-09-13 12:54:18 -0500

    US122_Vehicle Odometer Test Completed

--- src/test/java/com/transmuda/pages/VehicleOdometerPage.java
+++ src/test/java/com/transmuda/pages/VehicleOdometerPage.java
@@ -1,9 +1,9 @@
 package com.transmuda.pages;
 
-import com.transmuda.utilities.Driver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+
 
 public class VehicleOdometerPage extends BasePage {
 
